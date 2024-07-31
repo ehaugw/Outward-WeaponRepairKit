b0bab5aa2b1d82dce35577bf0339ad0a229de3bd
Eivind <ehaugw@gmail.com>
2024-07-31 10:10:28 +0200

    add weapon repair kit recipe to blacksmith ships

--- WeaponRepairKit.cs
+++ WeaponRepairKit.cs
@@ -7,12 +7,14 @@
     using System;
     using SideLoader;
     using System.IO;
+    using InstanceIDs;
+    using System.Linq;
 
     [BepInPlugin(GUID, NAME, VERSION)]
     public class WeaponRepairKit : BaseUnityPlugin
     {
         public const string GUID = "com.ehaugw.weaponrepairkit";
-        public const string VERSION = "1.0.1";
+        public const string VERSION = "1.0.2";
         public const string NAME = "Weapon Repair Kit";
         public static string ModFolderName = Directory.GetParent(typeof(WeaponRepairKit).Assembly.Location).Name.ToString();
 
@@ -22,11 +24,29 @@
             harmony.PatchAll();
 
             SL.OnPacksLoaded += OnPackLoaded;
+            SL.OnSceneLoaded += OnSceneLoaded;
         }
         private void OnPackLoaded()
         {
             WeaponRepairKitItem.MakeItem();
             WeaponRepairKitItem.MakeRecipe();
+        }
+
+        private void OnSceneLoaded()
+        {
+            foreach (GameObject obj in Resources.FindObjectsOfTypeAll<GameObject>().Where(x => x.name == "HumanSNPC_Blacksmith"))
+            {
+                if (obj.GetComponentsInChildren<GuaranteedDrop>()?.FirstOrDefault(table => table.ItemGenatorName == "Recipes") is GuaranteedDrop recipes)
+                {
+                    if (At.GetField<GuaranteedDrop>(recipes, "m_itemDrops") is List<BasicItemDrop> drops)
+                    {
+                        foreach (Item item in new Item[] { ResourcesPrefabManager.Instance.GetItemPrefab(IDs.weaponRepairKitRecipeID) })
+                        {
+                            drops.Add(new BasicItemDrop() { ItemRef = item, MaxDropCount = 1, MinDropCount = 1 });
+                        }
+                    }
+                }
+            }
         }
     }
 }
